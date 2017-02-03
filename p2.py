50fda0dde05d5c6435dea49b11239d366410c281
DAQIWEN <qiwen.guo@vanderbilt.edu>
2017-02-03 00:18:55 -0600

    get register things done

---
+++ p2.py
@@ -0,0 +1,14 @@
+from classes.publisher import *
+from random import randint
+import time
+
+#create a publisher and pass in intial configuration
+p1 = Publisher('127.0.0.1:5555',2,'topic a')
+p1.register()
+# keep publishing
+while True:
+	body = randint(0,9)
+	e1 = Event(p1.topic,body)
+	p1.publish(e1)
+	# sleep for 300ms
+	time.sleep(2)
