8eb00d8c7bd28f3d340567e7a93ee70a0273a5f5
Hamid Zr <dellydela@gmail.com>
2017-02-06 19:21:50 -0600

    feature: pass arguments to publishers via CLI

--- p1.py
+++ p1.py
@@ -1,13 +1,24 @@
 from classes.publisher import *
 from random import randint
+import sys # to get cli args
 import time
 
 #create a publisher and pass in intial configuration
-p1 = Publisher('127.0.0.1:5555',1,'topic a')
+
+if len(sys.argv)<4:
+	print 'pass ES_address, owner_strength and topic in this order as arguments'
+	sys.exit(1)
+
+# set the variables from the arguments passed	
+eventserver_address = sys.argv[1]
+owner_strength = sys.argv[2]
+topic = sys.argv[3]
+
+p1 = Publisher(eventserver_address,owner_strength,topic)
 p1.register()
 # keep publishing
 while True:
-	body = randint(0,9)
+	body = "body {}".format(randint(0,9))
 	e1 = Event(p1.topic,body)
 	p1.publish(e1)
 	# sleep for 300ms
