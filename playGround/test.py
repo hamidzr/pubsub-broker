918c453064eec387aabed98cd61931d963dfcca4
Hamid Zr <dellydela@gmail.com>
2017-02-15 12:01:22 -0600

    testing different consistent hashring packages #7

---
+++ playGround/test.py
@@ -0,0 +1,48 @@
+import threading
+import time
+import logging
+from hash_ring import *
+
+
+class DataStorage(object):
+    """docstring for DataStorage"""
+    def __init__(self,name):
+        super(DataStorage, self).__init__()
+        self.dic = {}
+        self.name = name
+    def __str__(self):
+        return self.name # whatever we wanna hash
+    """ set a key value
+    returns true if succeed """
+    def set(self,key,value):
+        self.dic[key] = value
+        return key, value + ' successfully added to DataStorage ' + self.name
+    def get(key):
+        return self.dic[key]
+
+
+ds1 = DataStorage('ds1')
+ds1.set('ali','30')
+        
+ds2 = DataStorage('ds2')
+ds2.set('lisa','25')
+
+ds3 = DataStorage('ds3')
+ds3.set('jack','25')
+
+ds4 = DataStorage('ds4')
+ds4.set('jhon','25')
+
+
+ring = HashRing([ds1, ds2, ds3, ds4]) # pass stringalbe items
+
+topic = 'a'
+print 'topic',topic,'node position', ring.get_node_pos(topic)
+print 'topic',topic,'node ', ring.get_node(topic)
+iterator =  ring.iterate_nodes(topic, True)
+for node in iterator:
+	print node
+
+# mc = MemcacheRing(['127.0.0.1:11212'])
+# mc.set('hello', 'world')
+# print mc.get('hello')
