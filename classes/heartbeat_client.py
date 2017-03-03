2138110a264ef44167cbe414e624bac8d7222073
SuveniTangnu <suveni.tangnu@vanderbilt.edu>
2017-03-02 18:20:06 -0600

    Changes to accept the hash ring nodes for PubSub from ES

--- classes/heartbeat_client.py
+++ classes/heartbeat_client.py
@@ -10,14 +10,14 @@ class heartbeatClient (threading.Thread):
 
 	daemon = True # make it a deamon
 
-	def __init__(self, pId, servAddr):
+	def __init__(self, pId, servAddr,pubSub):#suveni
 		threading.Thread.__init__(self)
 		self.pId = pId
 		self.servAddr = servAddr
 		context = zmq.Context()
 		self.socket = context.socket(zmq.REQ)
 		self.socket.connect("tcp://"+getHbServerFromAddress(servAddr))
-
+		self.pubSub = pubSub #suveni
 	def run(self):
 		logging.basicConfig(level=logging.INFO)
 		logger = logging.getLogger(__name__) #Q will this make it shared between all objects?
@@ -27,7 +27,11 @@ class heartbeatClient (threading.Thread):
 		logger.info( 'we are alive - heartbeating')
 		while True:
 			self.socket.send(b"{}".format(self.pId))
-			ack = self.socket.recv()
-			logger.info( ack)
+			#ack = self.socket.recv() suveni
+			inputNode = self.socket.recv() #suveni
+ 			self.pubSub.nodes = eval(inputNode)#.decode() suveni
+			for name in self.pubSub.nodes:
+				print ', '.join(str(item) for item in name)
+			logger.info( 'received the nodes from the eventserver ring') #suveni
 			time.sleep(5)	
 		logger.info( 'heartbeating stopped')
