834d05c4d3d8ed5680e3f2cc3a648732f3286716
DAQIWEN <qiwen.guo@vanderbilt.edu>
2017-03-16 02:28:01 -0500

    Simply working, may not be robust

--- s1.py
+++ s1.py
@@ -3,20 +3,25 @@ from classes.event import *
 import sys # to get cli args
 import datetime
 import logging
+import json
 if len(sys.argv) == 3:
 	esAddr = sys.argv[1]
 	topic = sys.argv[2]
 else:
 	logging.debug( 'no arguments provided, resorting to defaults')
-	esAddr = '127.0.0.1'
+	esAddr = '127.0.0.1:5555'
 	topic = 'book'
 
 #create a subscriber
-s1 = Subscriber(esAddr)
+s1 = Subscriber(esAddr,topic)
 # set the variables from the arguments passed
 #subscribe to a topic
 srvAddress = s1.lookup(topic)
-s1.register(topic, srvAddress)
+registered=s1.register(topic, srvAddress)#subcorrect
+while not registered:
+	srvAddress = s1.lookup(topic)
+	registered = s1.register(topic, srvAddress)  # subcorrect
+	time.sleep(2)
 s1.subscribe(topic)
 
 
@@ -26,5 +31,15 @@ logger.addHandler(hdlr)
 logger.setLevel(logging.INFO)
 
 while True:
-	msg = s1.socket.recv()
-	logger.info('recieved: '+ msg)
+	msg = s1.subsocket.recv()
+
+	logger.info('recieved: ' + msg)
+	#print('msg after split: ')
+	#print(msg.split(' '))
+	if msg.split(' ')[1] == '-1':
+		print('I received ES is dead')
+		srvAddress = s1.lookup(topic)
+		alive = s1.register(topic, srvAddress)
+		s1.subscribe(topic)
+
+
